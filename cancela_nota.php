f5c078fb92afd4ba07ddf8e01bd06a97850339c6
roberto.porto1981 <roberto.porto@outlook.com>
2020-03-12 17:43:29 -0300

    Testes

--- cancela_nota.php
+++ cancela_nota.php
@@ -9,10 +9,9 @@ $sql = "DELETE FROM itensnota where numeroitensnota = 0";
   
 echo"<script language='javascript' type='text/javascript'>alert('Cupom cancelado');window.location.href='pdv.php'</script>";
 
+mysqli_query($conexao,$sql) or die("Nao foi possivel cancelar a nota");
 
- mysqli_query($conexao,$sql) or die("Nao foi possivel cancelar a nota");
- 
- mysqli_close($conexao);
+mysqli_close($conexao);
 
 ?>
 
